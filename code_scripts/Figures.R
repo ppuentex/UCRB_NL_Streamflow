d3327d70b1d8501d32c04ad488b1b59e2a3dc7ce
Patricia <ppuente@email.arizona.edu>
2023-10-08 18:50:56 -0700

    Final Edits and code clean up

--- code_scripts/Figures.R
+++ code_scripts/Figures.R
@@ -44,7 +44,8 @@ upper_sig_list = c(0.77, 0.73, 0.61, 0.61, 0.68, 0.92, 0.95)
 #make labels for area fraction plots 
 llabels = c("(c)", "(e)", "(f)", "(b)", "(d)", "(a)", "(g)")
 
-#high and low predictability years
+#high and low predictability years [grabbed from high-low_pred_years.csv]
+#by getting the years that fall above / below +0.05 / -0.05 and are not close to each other
 low_pred = list(c(1933,1998), #greenriverwy
                 c(), #greenriverut
                 c(1987,1844,1600, 1653), #glenwood
@@ -61,15 +62,18 @@ high_pred = list(c(1820,1872,1692,1650), #greenriverwy
                  c(1787,1882,1941,1656), #leesferry-short
                  c(1161,861,1021,1256)) #leesferry-long
 
-
-#####
-#single figures 
+#single figures data load 
 leesferry_long_df = read.csv("./Output_Data/leesferry-long_ts.csv", header = TRUE)
 leesferry_short_df = read.csv("./Output_Data/leesferry-short_ts.csv", header = TRUE)
 
+sf_mean = mean(leesferry_long_df[,flow_col]/scale)
+sf_sd = sd(leesferry_long_df[,flow_col]/scale)
+#this data signal is expressed in MaF 
+data_signal = scale(leesferry_long_df[,signal_col]) * sf_sd + sf_mean
+
 #indexes for non-na values
 val_i=which(!is.na(leesferry_long_df$avgLLE))
-#grab index of pre, post, and observed years 
+#grab index of pre, post, and observed years [used for box plots and scatterplots]
 #762 - 1489
 pre_i <- c(val_i[1]:which(leesferry_long_df$year==1489))
 #1490 - 1905
@@ -77,15 +81,17 @@ post_i <- c((which(leesferry_long_df$year==1490)):which(leesferry_long_df$year==
 #1906 - 2020
 obs_i <- c(which(leesferry_long_df$year==1906):tail(val_i, n=1))
 
-#Figure 1 (a) [once the paper is ready for submission, this is how it should be labeled]
-#temp: leesferry-long time series for showing flow only 
-#uncomment comments to label the first figure 
+#get the years of high and low predictability for leesferry-long only 
+low_list = as.integer(low_pred[[7]])
+high_list = as.integer(high_pred[[7]])
+
+#######Figure 2 (a)#######
+#leesferry-long time series for showing flow only 
 png(paste(graphdir,"leesferry-long_flowts_1.png",sep = ""), width = 400, height = 150, units='mm', res = 300)
 par(mfrow=c(1,1),mar=c(4,5,3,1), mgp = c(2.5, 1, 0), cex.axis = 2, cex.lab =2)
 plot(leesferry_long_df[,yr_col], leesferry_long_df[,flow_col]/scale, ylim = c(0, range(leesferry_long_df[,flow_col]/scale)[2]), 
      type = 'l', lwd = 2.5, 
      col= colrs[6], 
-     #col = 'black',
      panel.first = grid(NULL,NULL, lty = 3, col = "grey"),
      xlab = "Years", ylab = "Streamflow (MaF)", cex.lab = 2, 
      cex.axis = 2)
@@ -95,7 +101,8 @@ mtext(side=3,"(a)",adj=0,cex=2)
 legend("bottomright", legend="leesferry-long", lwd = 3, lty = 1, col = colrs[6], bty = 'n', cex =2)
 dev.off()
 
-#temp: all time series for showing flow only
+#######Figure 2 (b)#######
+#all gauge time series for showing flow only
 png(paste(graphdir,"allgauge_flowts.png",sep = ""), width = 400, height = 150, units='mm', res = 300)
 par(mfrow=c(1,1),mar=c(4,5,3,1), mgp = c(2.5, 1, 0), cex.axis = 2, cex.lab =2)
 plot(leesferry_short_df[,yr_col], leesferry_short_df[,flow_col]/scale, ylim = c(0, range(leesferry_short_df[,flow_col]/scale)[2]), 
@@ -110,15 +117,25 @@ par(fig=c(0, 1, 0, 1), oma=c(0, 0, 0, 0), mar=c(0, 0, 0, 0), new=TRUE)
 legend("topright",horiz = T, legend=c(names[1],names[2],names[3],names[4], names[5],names[6]), lwd = 3, lty = 1, col = c(colrs[1],colrs[2],colrs[3],colrs[4],colrs[5],colrs[6]), bty = 'n', cex =1.3)
 dev.off()
 
+#######Figure 4 (a)#######
+png(paste(graphdir,"leesferry-long_flowts.png",sep = ""), width = 400, height = 150, units='mm', res = 300)
+par(mfrow=c(1,1),mar=c(4,5,3,1), mgp = c(2.5, 1, 0), cex.axis = 2, cex.lab =2)
+plot(leesferry_long_df[,yr_col], leesferry_long_df[,flow_col]/scale, ylim = c(0, range(leesferry_long_df[,flow_col]/scale)[2]), 
+     type = 'l', lwd = 2.5, 
+     col= 'black', 
+     panel.first = grid(NULL,NULL, lty = 3, col = "grey"),
+     xlab = "Years", ylab = "Streamflow (MaF)", cex.lab = 2, 
+     cex.axis = 2)
+grid (NULL,NULL, lty = 3, col = "grey")
+mtext(side=3,"(a)",adj=0,cex=2)
+dev.off()
+
+#######Figure 4 (b) & (c)#######
 #creates wavelet plot 
 wplot = wplot(leesferry_long_df[,flow_col]/scale, 762, name = "leesferry-long_PS", graphdir=graphdir)
 
-#temp: leesferry-long signal time series for wavelet figure 
-sf_mean = mean(leesferry_long_df[,flow_col]/scale)
-sf_sd = sd(leesferry_long_df[,flow_col]/scale)
-#this data signal is expressed in MaF 
-data_signal = scale(leesferry_long_df[,signal_col]) * sf_sd + sf_mean
-
+#######Figure 4 (d)#######
+#leesferry-long signal time series for wavelet figure 
 png(paste(graphdir,"leesferry-long_signalts.png",sep = ""), width = 400, height = 150, units='mm', res = 300)
 par(mfrow=c(1,1),mar=c(4,5,3,1), mgp = c(2.5, 1, 0), cex.axis = 2, cex.lab =2)
 plot(leesferry_long_df[,yr_col], data_signal, ylim = c(0, range(data_signal)[2]), 
@@ -128,6 +145,7 @@ plot(leesferry_long_df[,yr_col], data_signal, ylim = c(0, range(data_signal)[2])
 mtext(side=3,"(d)",adj=0,cex=2)
 dev.off()
 
+#######Figure 4 (e)#######
 png(paste(graphdir,"leesferry-long_sawp.png",sep = ""), width = 400, height = 150, units='mm', res = 300)
 par(mfrow=c(1,1),mar=c(4,5,3,1), mgp = c(2.5, 1, 0), cex.axis = 2, cex.lab =2)
 plot(leesferry_long_df[,yr_col], leesferry_long_df[,sawp_col], 
@@ -137,7 +155,7 @@ plot(leesferry_long_df[,yr_col], leesferry_long_df[,sawp_col],
 mtext(side=3,"(e)",adj=0,cex=2)
 dev.off()
 
-
+#######Figure 7 (a),(b),(c)#######
 #temp: leesferry-long streaflow, signal, avg LLE 
 png("./Figures/leesferry-long_ts.png", width = 9, height = 9, units='in', res = 300)
 par(mfrow=c(3,1),mar=c(4,4,2,1), mgp = c(2, .6, 0))
@@ -161,65 +179,23 @@ plot(leesferry_long_df[,yr_col], leesferry_long_df[,avg_LLE_col], ylim = c(-0.4,
 abline(h = 0.05, lty =2)
 abline(h = -0.05)
 
-#finds peak indexes 
-p_i=findPeaks(leesferry_long_df[,avg_LLE_col]) 
-#get the values of LLE for peak indexes 
-peak_values=leesferry_long_df[p_i,avg_LLE_col] 
-#sort these values in decreasing order (+ lle to - lle)
-sort_pval = sort(peak_values, index.return = TRUE, decreasing = TRUE)
-#create peak matrix to store year and avg LLE value
-peak_mat = matrix(0,length(sort_pval$x),2)
-peak_mat[,1] = leesferry_long_df[p_i[sort_pval$ix], yr_col]
-peak_mat[,2] = sort_pval$x
 
-#finds valley indexes 
-v_i = findValleys(leesferry_long_df[,avg_LLE_col])
-#get the values of LLE for valley indexes 
-valley_values = leesferry_long_df[v_i,avg_LLE_col] 
-#sort these values in decreasing order (+ lle to - lle)
-sort_vval = sort(valley_values, index.return = TRUE, decreasing = FALSE)
-#create peak matrix to store year and avg LLE value
-valley_mat = matrix(0,length(sort_vval$x),2)
-valley_mat[,1] = leesferry_long_df[v_i[sort_vval$ix], yr_col]
-valley_mat[,2] = sort_vval$x
-
-#I think we should do this ourselves and change it for every case
-#looking at the matrix created above
-peaks = list()
-valleys = list()
-#this works only for leesferry-long 
-#check that the peaks and valleys are not next to each other (alteast 19 years apart)
-for(i in 1:12){
-  if(i==1 & abs(v_i[sort_vval$ix[i]] - v_i[sort_vval$ix[i+1]]) < 19){
-    valleys <- append(valleys, i)
-  }
-  else if(abs(v_i[sort_vval$ix[i]] - v_i[sort_vval$ix[i+1]]) > 19){
-    valleys <- append(valleys, i)
-  }
-}
-for(i in 1:12){
-  if(abs(p_i[sort_pval$ix[i]] - p_i[sort_pval$ix[i+1]]) > 19){
-    peaks <- append(peaks, i)
-  }
-}
-valleys <- as.numeric(valleys)
-peaks <- as.numeric(peaks)
 #plot the peaks and valleys
 for(i in 1:4){
-  points(leesferry_long_df[p_i[sort_pval$ix[peaks[i]]],yr_col], leesferry_long_df[p_i[sort_pval$ix[peaks[i]]],avg_LLE_col], col="#993300", pch=15, cex= 2)
-  points(leesferry_long_df[v_i[sort_vval$ix[valleys[i]]],yr_col], leesferry_long_df[v_i[sort_vval$ix[valleys[i]]],avg_LLE_col], col="#336699", pch=15, cex= 2)
+  points(leesferry_long_df[(which(leesferry_long_df[,yr_col]==low_list[i])),yr_col], leesferry_long_df[(which(leesferry_long_df[,yr_col]==low_list[i])),avg_LLE_col], col="#993300", pch=15, cex= 2)
+  points(leesferry_long_df[(which(leesferry_long_df[,yr_col]==high_list[i])),yr_col], leesferry_long_df[(which(leesferry_long_df[,yr_col]==high_list[i])),avg_LLE_col], col="#336699", pch=15, cex= 2)
 }
 
 legend("bottomright", legend=c("<-0.05 (high predictability)", ">0.05 (low predictability)"), 
        col=c("black"), lwd=c( 1, 1), lty = c(1,2), cex=1.5, bty="n", border=F)
-#    col=c("black","#999966", "#999999"), lwd=c(2, 1, 1), lty = c(1,1,2), cex=1.0, bty="n", border=F)
 legend("bottom", legend=c("high predictability", "low predictability"),
        col=c("#336699","#993300"),cex=1.5, pch=15, pt.cex = 2, bty="n", border=F)
 mtext(side=3,"(c)",adj=0,cex=1.3)
 
 dev.off()
 
-#temp: leesferry - long scatterplots 
+#######Figure 12 (a)-(h)#######
+#leesferry - long scatterplots 
 all_avg<-ggplot(data=NULL, aes(leesferry_long_df$avgLLE[val_i], leesferry_long_df$flow_mean[val_i], col = leesferry_long_df$year[val_i])) + 
   stat_poly_line(se = FALSE, color = 'black') + stat_correlation() + geom_point(size=1) +
   scale_color_gradient(high = "#000033", low = "#CCCCFF") + 
@@ -243,7 +219,6 @@ post_avg<- ggplot(data=NULL, aes(leesferry_long_df$avgLLE[post_i], leesferry_lon
   theme(legend.title =element_text(size = 13), axis.title=element_text(size=15),plot.title = element_text(size=15), legend.text =element_text(size = 13),axis.text.x = element_text(size = 13), axis.text.y = element_text(size = 13)) +
   guides(colour="none") #turns off spectrum on side 
 
-
 obs_avg <- ggplot(data=NULL, aes(leesferry_long_df$avgLLE[obs_i], leesferry_long_df$flow_mean[obs_i], col = leesferry_long_df$year[obs_i])) + 
   xlim(range(na.omit(leesferry_long_df$avgLLE))) + ylim(range(na.omit(leesferry_long_df$flow_mean))) + 
   stat_poly_line(se = FALSE, color = 'black') + stat_correlation() + geom_point(size=1) +
@@ -251,7 +226,6 @@ obs_avg <- ggplot(data=NULL, aes(leesferry_long_df$avgLLE[obs_i], leesferry_long
   labs(title = '(g)' , x ="Average LLE", y = "Flow Average (MAF)") +
   theme(legend.title =element_text(size = 13), axis.title=element_text(size=15),plot.title = element_text(size=15), legend.text =element_text(size = 13),axis.text.x = element_text(size = 13), axis.text.y = element_text(size = 13)) +
   guides(colour="none") #turns off spectrum on side 
-
 
 #var SF and avg LLE 
 all_var<-ggplot(data=NULL, aes(leesferry_long_df$avgLLE[val_i], leesferry_long_df$flow_var[val_i], col = leesferry_long_df$year[val_i])) + 
@@ -287,32 +261,20 @@ png(paste(graphdir,"leesferry-long_scatterplots.png", sep=""), width = 220, heig
 dev.off()
 
 
-#temp: leesferry-long avg LLE, roll average, roll variance
-#ranges for y axis
-avglle_l = round((range(na.omit(leesferry_long_df[,avg_LLE_col]))[1]-0.08), digit = 1)
-avglee_u = round((range(na.omit(leesferry_long_df[,avg_LLE_col]))[2]+0.08), digit = 1)
-mean_l = round((range(na.omit(leesferry_long_df[,roll_mean_col]))[1] - 0.2), digit = 1)
-mean_u = round((range(na.omit(leesferry_long_df[,roll_mean_col]))[2] + 0.2), digit = 1)
-var_l = round((range(na.omit(leesferry_long_df[,roll_var_col]))[1] - 0.2), digit = 1)
-var_u = round((range(na.omit(leesferry_long_df[,roll_var_col]))[2] + 0.2), digit = 1)
-high_pred_col = "#99CCFF"
-low_pred_col = "#FF9999"
-range_val = 19
-#range_val = 19 #+ - for window of center avgLLE, var, avg 
-low_list = as.integer(low_pred[[7]])
-high_list = as.integer(high_pred[[7]])
+#######Figure 14 (a)-(h)#######
+range_val = 19 #half of 39 from Table 2
 
-#high predictability 40 year epochs
-LLE_lowerbound1 = high_list[1]-range_val-1; LLE_upperbound1 = high_list[1]+range_val
-LLE_lowerbound2 = high_list[2]-range_val-1; LLE_upperbound2 = high_list[2]+range_val
-LLE_lowerbound3 = high_list[3]-range_val-1; LLE_upperbound3 = high_list[3]+range_val
-LLE_lowerbound4 = high_list[4]-range_val-1; LLE_upperbound4 = high_list[4]+range_val
+#high predictability initial year in 40 year epoch
+LLE_lowerbound1 = high_list[1]-range_val-1 
+LLE_lowerbound2 = high_list[2]-range_val-1 
+LLE_lowerbound3 = high_list[3]-range_val-1
+LLE_lowerbound4 = high_list[4]-range_val-1
 
-#low predictability 40 year epochs
-LLE_lowerbound5 = low_list[1]-range_val-1; LLE_upperbound5 = low_list[1]+range_val
-LLE_lowerbound6 = low_list[2]-range_val-1; LLE_upperbound6 = low_list[2]+range_val
-LLE_lowerbound7 = low_list[3]-range_val-1; LLE_upperbound7 = low_list[3]+range_val
-LLE_lowerbound8 = low_list[4]-range_val-1; LLE_upperbound8 = low_list[4]+range_val
+#low predictability initial year in 40 year epoch
+LLE_lowerbound5 = low_list[1]-range_val-1
+LLE_lowerbound6 = low_list[2]-range_val-1
+LLE_lowerbound7 = low_list[3]-range_val-1
+LLE_lowerbound8 = low_list[4]-range_val-1
 
 #figure windows - avg LLE, mean, var 
 #initial year of the centering 40 yr epoch
@@ -343,13 +305,14 @@ ltys <- 1
 lwds <- 1
 
 labels = c('(a)', '(b)','(c)', '(d)')
+labelss = c('(e)', '(f)','(g)', '(h)')
 
-png(paste(graphdir,"high_pred_trajectories.png", sep=""), width = 8, height = 8, units='in', res = 300)
+png(paste(graphdir,"leesferry-long_high_pred_trajectories.png", sep=""), width = 8, height = 8, units='in', res = 300)
 par(mfrow=c(2,2), oma = c(0.5,0.5,0.5,0.5))
 for(j in 1:4){
   i=high_list[j]
   rnge=index.mat[i,1]:index.mat[i,2]
-  my.plot<-scatterplot3d(embb[,1],embb[,2],embb[,3],type="l",xlab=xlb ,ylab=ylb,zlab=zlb,color="gray",lwd=2,axis = TRUE, box = FALSE, main = paste(indexs[i],'-', indexs[i]+39, sep=" "))
+  my.plot<-scatterplot3d(embb[,1],embb[,2],embb[,3],type="l",xlab=xlb ,ylab=ylb,zlab=zlb,color="gray", cex.lab = 1.1,cex.axis = 1.1,lwd=2,axis = TRUE, box = FALSE, main = paste(indexs[i],'-', indexs[i]+39, sep=" "))
   my.plot$points3d(embb[rnge,1],embb[rnge,2],embb[rnge,3],col=high_pred_col,type="l",lwd=2,lty = ltys)
   my.plot$points3d(embb[(index.mat[i,1]),1],embb[(index.mat[i,1]),2],embb[(index.mat[i,1]),3],col=high_pred_col,type="p",lwd=2,lty = ltys)
   mtext(side=3,paste(labels[j]),adj=0,line = -1.5,cex=1.3)
@@ -357,19 +320,20 @@ for(j in 1:4){
 #mtext("High Predictability trajectories", side = 1, outer = T, line=0)
 dev.off()
 
-png(paste(graphdir,"low_pred_trajectories.png", sep=""), width = 8, height = 8, units='in', res = 300)
+png(paste(graphdir,"leesferry-long_low_pred_trajectories.png", sep=""), width = 8, height = 8, units='in', res = 300)
 par(mfrow=c(2,2), oma = c(0.5,0.5,0.5,0.5))
 for(j in 1:4){
   i=low_list[j]
   rnge=index.mat[i,1]:index.mat[i,2]
-  my.plot<-scatterplot3d(embb[,1],embb[,2],embb[,3],type="l",xlab=xlb ,ylab=ylb,zlab=zlb,color="gray",lwd=2,axis = TRUE, box = FALSE, main = paste(indexs[i],'-', indexs[i]+39, sep=" "))
+  my.plot<-scatterplot3d(embb[,1],embb[,2],embb[,3],type="l",xlab=xlb ,ylab=ylb,zlab=zlb,color="gray",cex.lab = 1.1,cex.axis = 1.1, lwd=2,axis = TRUE, box = FALSE, main = paste(indexs[i],'-', indexs[i]+39, sep=" "))
   my.plot$points3d(embb[rnge,1],embb[rnge,2],embb[rnge,3],col=low_pred_col,type="l",lwd=2,lty = ltys)
   my.plot$points3d(embb[(index.mat[i,1]),1],embb[(index.mat[i,1]),2],embb[(index.mat[i,1]),3],col=low_pred_col,type="p",lwd=2,lty = ltys)
-  mtext(side=3,paste(labels[j]),adj=0,line = -1.5,cex=1.3)
+  mtext(side=3,paste(labelss[j]),adj=0,line = -1.5,cex=1.3)
 }
 dev.off()
 
-#temp: predictability for all locations 
+#######Figure 10 (a)&(b)#######
+#predictability for all locations 
 epoch = 1:12
 #order c("greenriverwy", "greenriverut", "glenwood", "gunnison", "cisco", "leesferry-short")
 lag_pick = c(3,2,3,3,2,2)
@@ -430,7 +394,11 @@ mtext("Low Predictability trajectory [1980-1999]", side = 1, outer = T, line=0, 
 mtext("(b)", side = 3, outer = T, line = 0, adj = 0, cex=2)
 dev.off()
 
-#temp: boxplots
+
+#######Figure 13 (a)&(b)#######
+#boxplots
+
+#grab indices of where avg LLE falls below -0.05 and above 0.05 
 high_pred_i <- which(leesferry_long_df[,avg_LLE_col] < -0.05)
 low_pred_i <- which(leesferry_long_df[,avg_LLE_col] > 0.05)
 
@@ -536,8 +504,8 @@ mtext("Predictability", side = 1, outer = T, line = 2.5, adj = 0.5, cex=1.2)
 box()
 dev.off()
 
-#####
-#temp: correlation plots of streamflow, signal, and average LLE
+#######Figure 3, Figure 6, Figure 9#######
+#correlation plots of streamflow, signal, and average LLE
 #Loading all gauge data and storing it in a dataframe without opening all
 cols = c(flow_col, signal_col, avg_LLE_col)
 corr_name = c('flow', 'signal', 'avgLLE')
@@ -556,7 +524,7 @@ for(i in 1:3){
   Multp_plot = corrplot(cor(na.omit(Multi_df)), method = 'number',tl.col = 'black',is.corr = T, col.lim = c(plot_limit[i],1), tl.srt = 45, type = 'lower', col= COL2("BrBG"))
   dev.off()
 }
-
+#######Figure S2#######
 #scatterplots for all locations 
 for(name in names_sub){
   data_df = read.csv(paste(datadir,name,"_ts.csv", sep = ""), header = TRUE)
@@ -582,7 +550,8 @@ for(name in names_sub){
   dev.off()
 }
 
-#temp: predictability windows only for each gauge
+#######Figure 8#######
+#predictability windows only for each gauge
 png(paste(graphdir,"pred_windows.png", sep=""), width = 200, height = 200, units='mm', res = 300)
 par(mfcol=c(6,1), mar=numeric(4), oma = c(6, 6, 1, 4),  mgp = c(2, .6, 0)) #oma(bottom,left,top,right)
 for(name in names){
@@ -634,12 +603,13 @@ for(name in names){
   }
 }
 
-
+#######Figure 5(a)-(f), Figure 11, and Figure S1 #######
 #Multiple figures - area fraction, and avgLLE, roll mean, roll variance comparison w/ windows
 for(name in names){
   i = which(name==names)
   data_df = read.csv(paste(datadir,name,"_ts.csv", sep = ""), header = TRUE)
-  #temp: area fraction plots 
+  
+  #area fraction plots 
   areafrac = wplot_area(data_df[,flow_col],startY_list[i],dj=0.025,u.sig=upper_sig_list[i],name=name, graphdir, letter_label = llabels[i])
   
   #graph y-axis ranges 
@@ -677,7 +647,7 @@ for(name in names){
   LLE_lowerbound8 = low_list[4]-range_val-1; LLE_upperbound8 = low_list[4]+range_val
   
   
-  #temp: figure with avgLLE, rolling average and rolling variance with windows
+  #figure with avgLLE, rolling average and rolling variance with windows
   png(paste(graphdir, name, "_ts_comp.png", sep=""), width = 9, height = 9, units='in', res = 300)
   par(mfcol=c(3,1), mar=numeric(4), oma = c(6, 6, 4, 4),  mgp = c(2, .6, 0)) #oma(bottom,left,top,right)
   plot(data_df[,yr_col], data_df[,avg_LLE_col], type = 'l', 
